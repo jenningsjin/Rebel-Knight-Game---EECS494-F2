8c0d9f84066d7153a8d24d288f640e72e74c50a5
Ben Toll <tollben@umich.edu>
2016-03-26 16:31:31 -0400

    Added main menu (alpha)

---
+++ Assets/Scripts/ButtonExtras.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonExtras : MonoBehaviour {
+	public AudioClip buttonSound;
+	public AudioSource src;
+
+	// Use this for initialization
+	void Start () {
+		src = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void MakeHoverSound() {
+		Debug.Log ("HELLO");
+		src.PlayOneShot (buttonSound);
+	}
+}
