7155ca522a60309b8b202082c66dcea819188edd
asarav <asarav@umich.edu>
2016-03-26 22:08:29 -0400

    Started the Stampede

---
+++ Assets/Scripts/Jousting/Stampede/MoveIndividual.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class MoveIndividual : MonoBehaviour {
+
+    Rigidbody rigid;
+    public GameObject explosion;
+    public int state;
+    bool sentMsg = false;
+    //public GameObject hp_bar;
+
+    // Use this for initialization
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+
+    void Start()
+    {
+        state = 0;
+        //hp_bar = GameObject.Find("HP_Bar");
+    }
+
+    public void BeginGame()
+    {
+        ++state;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            state = 1;
+        }
+        switch (state)
+        {
+            case 0: // Before game start
+                break;
+            case 1: // Charge
+                rigid.AddForce(Vector3.forward * 20f);
+                Vector3 vel = rigid.velocity;
+                if (Input.GetKey(KeyCode.LeftArrow) && vel.x > -5f)
+                {
+                    vel.x -= 0.5f;
+                }
+                else if (Input.GetKey(KeyCode.RightArrow) && vel.x < 5f)
+                {
+                    vel.x += 0.5f;
+                }
+                rigid.velocity = vel;
+                break;
+            case 2: // After crossing the finish line
+                rigid.constraints = RigidbodyConstraints.FreezeAll;
+                if (!sentMsg)
+                {
+                    Fungus.Flowchart.BroadcastFungusMessage("LevelCleared");
+                    sentMsg = true;
+                }
+                break;
+            default:
+                Debug.Log("Unrecognized state");
+                break;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        rigid.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            //hp_bar.GetComponent<HealthBar>().decreaseHealth();
+        }
+    }
+
+    // TODO: consider moving this elsewhere
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ Assets/Scripts/Stampede.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stampede : MonoBehaviour {
+    public GameObject[] group;
+    public int size;
+	// Use this for initialization
+	void Start () {
+        if(size == 3)
+        {
+            Vector3 pos1 = this.transform.position;
+            pos1.z -= 4f;
+            group[0].transform.position = this.transform.position;
+            group[0] = GameObject.Instantiate(group[0]);
+            pos1.x = pos1.x - 1f;
+            pos1.z = pos1.z + 1f;
+            group[1].transform.position = pos1;
+            group[1] = GameObject.Instantiate(group[1]);
+            pos1.x = pos1.x + 3f;
+            group[2].transform.position = pos1;
+            group[2] = GameObject.Instantiate(group[2]);
+            transform.parent = group[0].transform;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
