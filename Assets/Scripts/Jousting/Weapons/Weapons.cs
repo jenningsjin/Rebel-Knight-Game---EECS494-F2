c7605a35442596f637b2300e744e25fb0291bc7a
matsuis <matsuis@umich.edu>
2016-03-28 20:17:19 -0400

    add weapon swap

---
+++ Assets/Scripts/Jousting/Weapons/Weapons.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapons : MonoBehaviour {
+
+    public GameObject[] weapons;
+    public int index = 0;
+    public int size = 1;
+
+	// Use this for initialization
+	void Start () {
+        weapons[0].SetActive(true);
+	    for (int i = 1; i < size; i++) {
+            weapons[i].SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.S)) {
+            weapons[index].SetActive(false);
+            index = (index + 1) % size;
+            weapons[index].SetActive(true);
+        }
+	}
+
+
+}
