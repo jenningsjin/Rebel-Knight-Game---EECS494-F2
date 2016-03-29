576c8db010f1ece0053f742cc668bf712f42321f
jenningsjin <jinjenni@umich.edu>
2016-03-29 15:51:12 -0400

    added second stage

---
+++ Assets/Enemy3.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy3 : MonoBehaviour {
+	Rigidbody rigid;
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody>();
+		//rigid.velocity.z = -20;
+		rigid.AddForce (Vector3.back * 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( rigid.velocity.z < 5) {
+				rigid.AddForce (Vector3.back * 5);
+		}
+	}
+
+	void OnCollisionEnter(Collision col) {        
+				Destroy (this.gameObject);
+	}
+}
