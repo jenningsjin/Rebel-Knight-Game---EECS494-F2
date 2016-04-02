34d19afc146c14a78b54f82fc737a153287e23aa
matsuis <matsuis@umich.edu>
2016-04-01 21:10:21 -0400

    add obstacles

---
+++ Assets/Scripts/Jousting/Obstacles/Door.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+    public float speed = 1;
+    public Vector3 vel;
+    Rigidbody rigid;
+	// Use this for initialization
+	void Start () {
+        rigid = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        MoveWall();
+	} 
+
+    void MoveWall() {
+        rigid.velocity = vel * speed;
+    }
+
+    void OnCollisionEnter(Collision c) {
+        if (c.gameObject.tag == "Switch") {
+            speed *= -1;
+        }
+        else if (c.gameObject.tag == "Knight") {
+            Debug.Log("hit player");
+        }
+        
+    }
+}
