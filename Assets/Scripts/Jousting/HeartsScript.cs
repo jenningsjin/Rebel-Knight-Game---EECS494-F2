a2c4dc1df926faf981c42d5805e14d3e9caec0da
Ben Toll <tollben@umich.edu>
2016-04-03 12:00:29 -0400

    Finished Hearts GUI

--- Assets/Scripts/Jousting/HeartsScript.cs
+++ Assets/Scripts/Jousting/HeartsScript.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class HeartsScript : MonoBehaviour {
+	public int index;
+	public GameObject player;
+	public bool sentMsg = false;
+	public Sprite [] hp_sprites;
 
 	// Use this for initialization
 	void Start () {
-	
+		index = 0;
 	}
 	
 	// Update is called once per frame
@@ -14,6 +19,17 @@ public class HeartsScript : MonoBehaviour {
 	}
 
 	public void decreaseHealth() {
-
+		++index;
+		if (index >= 5) { // Game Over
+			// Freeze the camera, disable player movement, and send a message
+			// to Fungus, so that Fungus displays the defeat Flowchart.
+			player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+			player.GetComponent<MoveKnight> ().state = 0;
+			if (!sentMsg) {
+				Fungus.Flowchart.BroadcastFungusMessage ("Defeat");
+				sentMsg = true;
+			}
+		}
+		gameObject.GetComponent<Image>().sprite = hp_sprites[index];
 	}
 }
