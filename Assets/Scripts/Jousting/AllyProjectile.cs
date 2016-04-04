e0afe4a99dd9ff1d6b0b043881bf918b5e11c9fe
Ben Toll <tollben@umich.edu>
2016-04-04 13:49:55 -0400

    Projectiles can now kill enemies

---
+++ Assets/Scripts/Jousting/AllyProjectile.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AllyProjectile : MonoBehaviour {
+	private Rigidbody rigid;
+	public GameObject explosion;
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody>();
+		Vector3 vel = MoveKnight.rigid.velocity;
+		//vel.y = 10f;
+		vel.z += 20f;
+		rigid.velocity = vel;
+		player = GameObject.Find ("Knight");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Mathf.Abs (this.transform.position.z - player.transform.position.z) > 30f) {
+			explosion.transform.position = this.transform.position;
+			GameObject.Instantiate (explosion);
+			Destroy (this.gameObject);
+		}
+	}
+
+	void OnCollisionEnter(Collision coll) {
+		if (coll.gameObject.CompareTag ("Obstacle") || coll.gameObject.CompareTag ("Enemy")) {
+			explosion.transform.position = this.transform.position;
+			GameObject.Instantiate (explosion);
+			Destroy (this.gameObject);
+		}
+	}
+}
