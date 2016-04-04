c51daaad7e1f54bac8daf5c48ca910f60ea220df
asarav <asarav@umich.edu>
2016-04-03 20:57:28 -0400

    Attack Feel. Slow Motion. Some Bug Fixes.

--- Assets/Scripts/Jousting/MoveKnight.cs
+++ Assets/Scripts/Jousting/MoveKnight.cs
@@ -5,27 +5,28 @@ using UnityEngine.SceneManagement;
 public class MoveKnight : MonoBehaviour {
     public static Rigidbody rigid;
     public GameObject explosion;
-	public int state;
-	public GameObject hp_bar;
-	public int jumpSpeed = 100;
-	public BoxCollider groundCollider;
+    public int state;
+    public GameObject hp_bar;
+    public int jumpSpeed = 100;
+    public BoxCollider groundCollider;
     private float moveTimer = 0.1f;
     public static int lane = 1;
     public static float leftLane = -4f;
     public static float rightLane = 4f;
     public static float midLane = 0f;
     private float maxSpeed = 20f;
-	bool sentMsg = false;
-	public bool grounded = true;
+    bool sentMsg = false;
+    public bool grounded = true;
     public static bool lanceReady = false;
     public GameObject lance;
-    float lanceTimer = 1f;
+    float lanceTimer = 0.2f;
     public GameObject person;
     public ParticleSystem particle;
     float healthTimer = 1f;
     bool tookDamage = false;
     bool left = false;
-
+    float attackDelay = 0.5f;
+    
     // Use this for initialization
     void Start () {
         rigid = GetComponent<Rigidbody>();
@@ -36,33 +37,48 @@ public class MoveKnight : MonoBehaviour {
         particle = GetComponent<ParticleSystem>();
         particle.enableEmission = false;
         lance.SetActive(false);
+        Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Obstacle"), LayerMask.NameToLayer("Default"), false);
+        Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Obstacle"), LayerMask.NameToLayer("MainCamera"), false);
+        Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Enemy"), LayerMask.NameToLayer("Default"), false);
+        Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Enemy"), LayerMask.NameToLayer("MainCamera"), false);
     }
 
 	public void BeginGame() {
 		++state;
 	}
-
-	// Update is called once per frame
-	void Update () {
-        moveTimer -= Time.deltaTime;
+    void FixedUpdate()
+    {
         if (lanceReady)
         {
             particle.enableEmission = true;
             lanceTimer -= Time.deltaTime;
-            
-            if(lanceTimer < 0)
+            if(lanceTimer < 0.2 && lanceTimer > 0)
+            {
+                Vector3 tmp = this.transform.eulerAngles;
+                tmp.y += 1f;
+                tmp.x -= 0.5f;
+                this.transform.eulerAngles = tmp;
+            }
+            if (lanceTimer < 0)
             {
                 particle.enableEmission = false;
-                lanceTimer = 1f;
-                //Time.timeScale = 1f;
+                lanceTimer = 0.2f;
+                Time.timeScale = 1f;
                 lanceReady = false;
                 lance.SetActive(false);
+                this.transform.eulerAngles = Vector3.zero;
             }
         }
+    }
+	// Update is called once per frame
+	void Update () {
+        moveTimer -= Time.deltaTime;
+
         switch (state) {
 		case 0: // Before game start
 			break;
 		case 1: // Charge
+                attackDelay -= Time.deltaTime;
             changeSpeed();
                 if (tookDamage)
                 {
@@ -132,11 +148,12 @@ public class MoveKnight : MonoBehaviour {
                     person.transform.position = personPos;
                     GameObject.Instantiate(person);
                     BoidController.flockSize--;
-                } else if(Input.GetKeyDown(KeyCode.Space) && lanceTimer > 0)
+                } else if(Input.GetKeyDown(KeyCode.Space) && lanceTimer > 0 && attackDelay < 0)
                 {
                     lanceReady = true;
                     lance.SetActive(true);
-                    //Time.timeScale = 0.8f;
+                    Time.timeScale = 0.25f;
+                    attackDelay = 0.5f;
                 }
 			break;
 		case 2: // After crossing the finish line
@@ -219,7 +236,7 @@ public class MoveKnight : MonoBehaviour {
         }
         else if(col.gameObject.tag == "Enemy" && lanceReady)
         {
-            lanceTimer = 0.2f;
+            lanceTimer = 0.05f;
         }
         if(col.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
         {
@@ -308,7 +325,7 @@ public class MoveKnight : MonoBehaviour {
             //Gradually return rotation to 0
             transform.Rotate(Vector3.right, -10f * Time.deltaTime * 8f);
         }
-        else if (transform.eulerAngles != Vector3.zero && !tookDamage)
+        else if (transform.eulerAngles != Vector3.zero && !tookDamage && !lanceReady)
         {
             transform.eulerAngles = Vector3.zero;
         }
