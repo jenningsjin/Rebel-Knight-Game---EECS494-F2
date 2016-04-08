9911a569861b69c93a87b4abb73a9609c26281cf
Ben Toll <tollben@umich.edu>
2016-04-08 18:21:47 -0400

    Committing boss level 2 start and test scenes

---
+++ Assets/Scripts/Enemy/FinalBossScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	int state = 0;
+	delegate void Attack();
+	Attack attackChoice;
+	Attack [] listOfGroundAttacks;
+	Attack [] listOfAirAttacks;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (state) {
+		case 0: // Moving on the ground
+			break;
+
+		case 1: // Flying overhead
+			break;
+
+		case 2: // Select Attack
+			break;
+
+		case 3: // Broadcast choice and attack
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	// Attack choices
+	void ShootFireball() {
+
+	}
+
+	void CreateWallOfFlames() {
+
+	}
+
+	void CreatePillarOfFire() {
+
+	}
+
+	void AirStrike() {
+
+	}
+
+	void MeteorShower() {
+
+	}
+
+}
