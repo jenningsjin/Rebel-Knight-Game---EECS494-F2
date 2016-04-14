d6165cbc29911822bbf69999ea47145480b4ad05
matsuis <matsuis@umich.edu>
2016-04-14 03:03:23 -0400

    bugfix

---
+++ Assets/Scripts/Jousting/Obstacles/CheckpointPit.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointPit : MonoBehaviour {
+    public GameObject[] checkpoints;
+    public float lastZ = 5;
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+        //lastZ = 5;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (player.transform.position.z > checkpoints[0].transform.position.z) {
+            lastZ = checkpoints[0].transform.position.z;
+        }
+        if (player.transform.position.z > checkpoints[1].transform.position.z) {
+            lastZ = checkpoints[1].transform.position.z;
+        }
+	}
+
+
+
+}
--- Assets/Scripts/Jousting/Obstacles/Pit.cs
+++ Assets/Scripts/Jousting/Obstacles/Pit.cs
@@ -7,6 +7,9 @@ using System.Collections;
 public class Pit : MonoBehaviour {
 
     public GameObject hp_bar;
+    public CheckpointPit checkpoint;
+    public GameObject player;
+    public Animator horse;
 
     // Use this for initialization
     void Start () {
@@ -20,8 +23,27 @@ public class Pit : MonoBehaviour {
 
     void OnCollisionEnter(Collision c) {
         if (c.gameObject.tag == "Knight") {
-            hp_bar.GetComponent<HeartsScript>().killPlayer();
+            hp_bar.GetComponent<HeartsScript>().decreaseHealth();
+            StartCoroutine(Respawn());
+
         }
     }
-    
+
+    IEnumerator Respawn() {
+
+        //yield return new WaitForSeconds(2);
+        Vector3 temp = Vector3.zero;
+        temp.y = 1.5f;
+        temp.z = checkpoint.lastZ;
+        player.transform.position = temp;
+        player.gameObject.GetComponent<MoveKnight>().state = 0;
+        player.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        MoveKnight.lane = 1;
+        horse.enabled = false;
+        yield return new WaitForSeconds(2);
+        horse.enabled = true;
+        player.gameObject.GetComponent<MoveKnight>().BeginGame();
+        
+    }
+
 }
