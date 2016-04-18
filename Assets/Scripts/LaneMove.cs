11b71232757f225e53317918252f1f45ad8733b3
asarav <asarav@umich.edu>
2016-04-17 22:59:52 -0400

    Added Lane Lines to Boss Levels and Tutorial

---
+++ Assets/Scripts/LaneMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaneMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = MoveKnight.rigid.position;
+        pos.x = 0;
+        pos.y = 0.5f;
+        this.transform.position = pos;
+	}
+}
