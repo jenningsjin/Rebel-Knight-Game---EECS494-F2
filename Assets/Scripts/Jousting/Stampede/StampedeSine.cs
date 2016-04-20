b556476b7166f18f0215d37d5c0e39528d30fbdf
jenningsjin <jinjenni@umich.edu>
2016-04-20 18:04:57 -0400

    updated stampede to run at different speeds

--- Assets/Scripts/Jousting/Stampede/StampedeSine.cs
+++ Assets/Scripts/Jousting/Stampede/StampedeSine.cs
@@ -2,16 +2,19 @@
 using System.Collections;
 
 public class StampedeSine : MonoBehaviour {
-	public float flutterRange = 0;
-
+	public float flutterRange = .001f;
+	public float flutterSpeed = 1f;
+	float time = 0f;
 	// Use this for initialization
 	void Start () {
 
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		time+=Time.deltaTime*flutterSpeed;
+		//time+=flutterSpeed;
+		float drift = Mathf.Sin(time) * flutterRange;
+		transform.Translate( drift , 0, 0 );	
 	}
 }
