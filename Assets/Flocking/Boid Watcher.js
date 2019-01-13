268d8a937fdb58fbc49bd4957599ab89df31ad3d
asarav <asarav@umich.edu>
2019-01-12 19:29:17 -0500

    Update and remove files so that the game can be built in webgl

--- Assets/Flocking/Boid Watcher.js
+++
@@ -1,8 +0,0 @@
-var boidController : Transform;
-
-function LateUpdate () {
-	if (boidController) {
-		var watchPoint : Vector3 = boidController.GetComponent("Boid Controller").flockCenter;
-		transform.LookAt(watchPoint+boidController.transform.position);	
-	}
-}
